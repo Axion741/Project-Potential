1b805121f875c2fe69cfdcbeb280c2d1ef98eb44
Unknown <k.ballagher@gmail.com>
2017-06-26 17:58:47 +0100

    Imported Mobile Templates and Menu System
    Imported custom asset pack from Glitch Garden Project as template.

---
+++ Project Potential/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+    public float autoLoadNextLevelAfter;
+    public static bool cameFromOptionsMenu = false;
+
+    private void Start()
+    {
+        if(autoLoadNextLevelAfter <= 0)
+        {
+            Debug.Log("Autoload disabled, use positive number in seconds");
+        }
+        else Invoke("LoadNextLevel", autoLoadNextLevelAfter);
+
+    }
+
+    public void LoadLevel(string name) {
+        if (SceneManager.GetActiveScene().buildIndex == 2)
+        {
+            cameFromOptionsMenu = true;
+            Debug.Log("Came from Options True)");
+        }else
+        {
+            cameFromOptionsMenu = false;
+            Debug.Log("Came from Options False");
+        }
+
+        Debug.Log("Level Load Requested for: " + name);
+        SceneManager.LoadScene(name);
+       
+    }
+
+    public void QuitRequest() {
+        Debug.Log("Quit Request logged");
+        Application.Quit();
+    }
+
+    public void LoadNextLevel()
+    {
+        SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
+       
+    }
+
+}
---
+++ Project Potential/Assets/Scripts/MusicManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour {
+
+    public AudioClip[] levelMusicChangeArray;
+    
+    private AudioSource audioSource;
+       
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        Debug.Log("DontDestroyOnLoad" + name);
+    }
+    // Use this for initialization
+    void Start () {
+        audioSource = GetComponent<AudioSource>();
+       //set volume to saved preference on start
+        audioSource.volume = PlayerPrefsManager.GetMasterVolume();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    private void OnLevelWasLoaded(int level)
+    {
+        AudioClip thisLevelMusic = levelMusicChangeArray[level];
+        Debug.Log("Playing clip" + thisLevelMusic);
+
+        if (thisLevelMusic && !LevelManager.cameFromOptionsMenu)
+        {//if some music attached
+            audioSource.clip = thisLevelMusic;
+            audioSource.loop = true;
+            audioSource.Play();
+        }
+    }
+
+    public void ChangeVolume (float volume)
+    {
+       audioSource.volume = volume;
+    }
+}
