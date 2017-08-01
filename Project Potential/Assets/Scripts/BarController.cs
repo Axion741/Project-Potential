9116b4eb2155f6904e304f46718095b345cc3d19
Unknown <k.ballagher@gmail.com>
2017-08-01 15:26:02 +0100

    Added Health Bar
    Added health bar with scripts based on player character health.

---
+++ Project Potential/Assets/Scripts/BarController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BarController : MonoBehaviour {
+
+    [SerializeField]
+    private float fillValue;
+
+    [SerializeField]
+    private Image bar;
+    
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        HandleBar();
+	}
+
+    private void HandleBar()
+    {
+        fillValue = HealthTest.playerCurrentHealth / HealthTest.playerMaxHealth;
+        bar.fillAmount = fillValue;
+    }
+}
---
+++ Project Potential/Assets/Scripts/HealthTest.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthTest : MonoBehaviour {
+
+    
+    public static float playerCurrentHealth;
+
+    public static float playerMaxHealth = 100;
+
+    private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+        playerCurrentHealth = playerMaxHealth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        print(playerCurrentHealth);
+
+        if (playerCurrentHealth > playerMaxHealth)
+        {
+            playerCurrentHealth = playerMaxHealth;
+        }
+        else if (playerCurrentHealth <= 0)
+        {
+            anim.SetTrigger("isDamaged");
+        }
+	}
+
+    public void DamageTaken()
+    {
+        playerCurrentHealth = playerCurrentHealth - 10;
+    }
+}
