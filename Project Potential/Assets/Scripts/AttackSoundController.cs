34f81ad092902f6731a675d9f285ff34254afb60
Unknown <k.ballagher@gmail.com>
2017-10-15 14:54:13 +0100

    Added Punch, Kick, Dash and Dodge Sounds
    Added sound effects to Punches, Kicks, Dodges and the Dash animation.

---
+++ Project Potential/Assets/Scripts/AttackSoundController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackSoundController : MonoBehaviour {
+
+    public AudioClip[] attackSounds;
+
+    private AudioSource audioSource;
+
+    // Use this for initialization
+    void Start () {
+        audioSource = GetComponent<AudioSource>();
+        //set volume to saved preference on start
+        audioSource.volume = PlayerPrefsManager.GetMasterVolume();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void FirstPunchSound()
+    {
+        audioSource.clip = attackSounds[0];
+        audioSource.Play();
+    }
+
+    public void SecondPunchSound()
+    {
+        audioSource.clip = attackSounds[1];
+        audioSource.Play();
+    }
+
+    public void KickSound()
+    {
+        audioSource.clip = attackSounds[2];
+        audioSource.Play();
+    }
+
+    public void DashSound()
+    {
+        audioSource.clip = attackSounds[3];
+        audioSource.Play();
+    }
+
+    public void DodgeSound()
+    {
+        audioSource.clip = attackSounds[4];
+        audioSource.Play();
+    }
+
+
+}
